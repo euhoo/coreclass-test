71c2e1b1a952e585d2941acb8b6301de619505f6
euhoo <euhoo@mail.ru>
2019-05-18 09:50:52 +0300

    add all inputs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,8 +26,8 @@ componentDidMount() {
       const id = e.target.value;
       this.setState({ id });
       const data = {
- id, value, name, page, perPage 
-};
+        id, value, name, page, perPage,
+      };
       console.log(data);
       const request = JSON.stringify(data);
       console.log(request);
@@ -48,38 +48,52 @@ componentDidMount() {
       const { store } = this.state;
       const { allIds, byId } = store;
       return (
-        <table className="table table-sm">
-          <thead>
-            <tr>
-              <th>
-                <input className="form-control" type="text" placeholder="id" onChange={this.idChange} />
-              </th>
-              <th><input className="form-control" type="text" placeholder="name" /></th>
-              <th>
-                <input className="form-control" type="text" placeholder="min" />
-                <input className="form-control" type="text" placeholder="max" />
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-sm-9">
+              Elements
+              <table className="table table-sm">
+                <thead>
+                  <tr>
+                    <th>id</th>
+                    <th>name</th>
+                    <th>value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  { allIds.map((item) => {
+                    const { id, name, value } = byId[item];
+                    return (
+                      <tr key={id}>
+                        <th scope="row">{id}</th>
+                        <td>{name}</td>
+                        <td>{value}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+            <div className="col-12 col-sm-3">
+              Sort & filter
+              <div>
+                Id:
+                <input className="form-control w-50" type="number" placeholder="id" onChange={this.idChange} />
+                Name:
+                <input className="form-control" type="text" placeholder="name" />
+                Value:
+                <input className="form-control w-50" type="number" placeholder="min" min="0" />
+                <input className="form-control w-50" type="number" placeholder="max" />
+                Count of pages:
+                <input className="form-control w-50" type="number" placeholder="pages" min="0" />
+                Count of elements per page:
+                <input className="form-control w-50" type="number" placeholder="per page" min="0" />
+              </div>
+            </div>
+          </div>
+        </div>
 
-              </th>
-            </tr>
-            <tr>
-              <th>id</th>
-              <th>name</th>
-              <th>value</th>
-            </tr>
-          </thead>
-          <tbody>
-            { allIds.map((item) => {
-              const { id, name, value } = byId[item];
-              return (
-                <tr key={id}>
-                  <th scope="row">{id}</th>
-                  <td>{name}</td>
-                  <td>{value}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+
       );
     }
 }
